089e51d8aefd0bc674a30cc220549685a3addae3
Pafonchick <pafonqwdert@gmail.com>
2025-04-19 07:49:44 +0500

    Add files via upload

---
+++ практика4/practice_5/Counter.vue
@@ -0,0 +1,24 @@
+<!-- КЛапука Павел БИУ-22-01 -->
+<template>
+  <div>
+    <h1>Счетчик: {{ count }}</h1>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'CounterPage',
+  setup() {
+    const count = ref(0);
+
+    const increment = () => count.value++;
+    const decrement = () => count.value--;
+
+    return { count, increment, decrement };
+  },
+});
+</script>
